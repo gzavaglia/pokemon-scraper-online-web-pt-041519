d62fee10b763586b468623b7a2b46c3a9234aba1
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-06-29 00:26:48 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -2,7 +2,7 @@ class Pokemon
   attr_accessor :name, :type, :id, :db 
   
   def initialize(arguments)
-    arguments.each do {|key, value| }
+    arguments.each do {|key, value| self.send()}
   end
   
 end
