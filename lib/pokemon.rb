5af92d1db36249acf6d6a21e903b508c96344a4b
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-06-29 00:34:11 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -5,14 +5,14 @@ class Pokemon
     arguments.each {|key, value| self.send(("#{key}="), value)}
   end
   
-  def self.save
+  def self.save(name, type, db)
     sql = <<-SQL
       INSERT INTO pokemon
       (name, type, db)
       VALUES (?,?)
     SQL
-    DB[:conn].execute(sql, self.name, self.type, self.db)
-    self
+    DB[:conn].execute(sql, name, type, db)
+    
   end
   
 end
